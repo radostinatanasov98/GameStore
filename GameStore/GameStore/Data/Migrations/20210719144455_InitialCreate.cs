692bc7e677f3d6deba94649338e71af63b3e32f8
radostinatanasov98 <radostin.atanasov.1998@protonmail.com>
2021-07-19 17:55:55 +0300

    Commiting files, which were forgotten in the previous commit.

---
+++ GameStore/GameStore/Data/Migrations/20210719144455_InitialCreate.cs
@@ -0,0 +1,380 @@
+﻿namespace GameStore.Data.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clients_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Publishers",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Publishers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Publishers_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClientGames",
+                columns: table => new
+                {
+                    ClientId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    GameId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ClientId1 = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    GameId1 = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientGames", x => new { x.ClientId, x.GameId });
+                    table.ForeignKey(
+                        name: "FK_ClientGames_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ClientGames_Clients_ClientId1",
+                        column: x => x.ClientId1,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Caption = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: true),
+                    Content = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    Rating = table.Column<int>(type: "int", nullable: false),
+                    GameId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    GameId1 = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    ClientId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ClientId1 = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Clients_ClientId1",
+                        column: x => x.ClientId1,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GameGenres",
+                columns: table => new
+                {
+                    GameId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    GenreId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    GameId1 = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    GenreId1 = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameGenres", x => new { x.GameId, x.GenreId });
+                    table.ForeignKey(
+                        name: "FK_GameGenres_Genres_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_GameGenres_Genres_GenreId1",
+                        column: x => x.GenreId1,
+                        principalTable: "Genres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Requirements",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CPU = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    GPU = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    VRAM = table.Column<int>(type: "int", nullable: false),
+                    RAM = table.Column<int>(type: "int", nullable: false),
+                    Storage = table.Column<int>(type: "int", nullable: false),
+                    OS = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    GameId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requirements", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    CoverImageUrl = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    MinimumRequirementsId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RecommendedRequirementsId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    PublisherId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    PublisherId1 = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    PegiRating = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Games_Publishers_PublisherId",
+                        column: x => x.PublisherId,
+                        principalTable: "Publishers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Games_Publishers_PublisherId1",
+                        column: x => x.PublisherId1,
+                        principalTable: "Publishers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Games_Requirements_MinimumRequirementsId",
+                        column: x => x.MinimumRequirementsId,
+                        principalTable: "Requirements",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Games_Requirements_RecommendedRequirementsId",
+                        column: x => x.RecommendedRequirementsId,
+                        principalTable: "Requirements",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientGames_ClientId1",
+                table: "ClientGames",
+                column: "ClientId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientGames_GameId",
+                table: "ClientGames",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientGames_GameId1",
+                table: "ClientGames",
+                column: "GameId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_UserId",
+                table: "Clients",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameGenres_GameId1",
+                table: "GameGenres",
+                column: "GameId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameGenres_GenreId",
+                table: "GameGenres",
+                column: "GenreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameGenres_GenreId1",
+                table: "GameGenres",
+                column: "GenreId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_MinimumRequirementsId",
+                table: "Games",
+                column: "MinimumRequirementsId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_PublisherId",
+                table: "Games",
+                column: "PublisherId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_PublisherId1",
+                table: "Games",
+                column: "PublisherId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_RecommendedRequirementsId",
+                table: "Games",
+                column: "RecommendedRequirementsId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Publishers_UserId",
+                table: "Publishers",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requirements_GameId",
+                table: "Requirements",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_ClientId",
+                table: "Reviews",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_ClientId1",
+                table: "Reviews",
+                column: "ClientId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_GameId",
+                table: "Reviews",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_GameId1",
+                table: "Reviews",
+                column: "GameId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ClientGames_Games_GameId",
+                table: "ClientGames",
+                column: "GameId",
+                principalTable: "Games",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ClientGames_Games_GameId1",
+                table: "ClientGames",
+                column: "GameId1",
+                principalTable: "Games",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Games_GameId",
+                table: "Reviews",
+                column: "GameId",
+                principalTable: "Games",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Games_GameId1",
+                table: "Reviews",
+                column: "GameId1",
+                principalTable: "Games",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GameGenres_Games_GameId",
+                table: "GameGenres",
+                column: "GameId",
+                principalTable: "Games",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GameGenres_Games_GameId1",
+                table: "GameGenres",
+                column: "GameId1",
+                principalTable: "Games",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Requirements_Games_GameId",
+                table: "Requirements",
+                column: "GameId",
+                principalTable: "Games",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Requirements_Games_GameId",
+                table: "Requirements");
+
+            migrationBuilder.DropTable(
+                name: "ClientGames");
+
+            migrationBuilder.DropTable(
+                name: "GameGenres");
+
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Publishers");
+
+            migrationBuilder.DropTable(
+                name: "Requirements");
+        }
+    }
+}
---
+++ GameStore/GameStore/Infrastructure/ApplicationBuilderExtensions.cs
@@ -0,0 +1,55 @@
+﻿namespace GameStore.Infrastructure
+{
+    using GameStore.Data;
+    using GameStore.Data.Models;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+    using System.Linq;
+
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder PrepareDatabase(
+            this IApplicationBuilder app)
+        {
+            using var scopedServices = app.ApplicationServices.CreateScope();
+
+            var data = scopedServices.ServiceProvider.GetService<GameStoreDbContext>();
+
+            data.Database.Migrate();
+
+            SeedGenres(data);
+
+            return app;
+        }
+
+        private static void SeedGenres(GameStoreDbContext data)
+        {
+            if (data.Genres.Any())
+            {
+                return;
+            }
+
+            data.Genres.AddRange(new[]
+            {
+                new Genre { Name = "RPG"},
+                new Genre { Name = "Shooter"},
+                new Genre { Name = "Action"},
+                new Genre { Name = "Mystery"},
+                new Genre { Name = "Strategy"},
+                new Genre { Name = "Sci-Fi"},
+                new Genre { Name = "Fantasy"},
+                new Genre { Name = "Sports"},
+                new Genre { Name = "Racing"},
+                new Genre { Name = "Platformer"},
+                new Genre { Name = "Third Person"},
+                new Genre { Name = "MMORPG"},
+                new Genre { Name = "MOBA"},
+                new Genre { Name = "Sandbox"},
+                new Genre { Name = "Simulator"},
+            });
+
+            data.SaveChanges();
+        }
+    }
+}
